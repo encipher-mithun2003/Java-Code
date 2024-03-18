4526a563e888b4fdff5d4358af42da577ddb8433
Mithun Varshan S <114509306+encipher-mithun2003@users.noreply.github.com>
2024-03-18 21:35:43 +0530

    Create Sum_of_positive.java

---
+++ Coding_Phase/Sum_of_positive.java
@@ -0,0 +1,12 @@
+// SUM OF POSITIVE NUMBERS using do-while loop
+public class DoWhile
+{    
+	public static void main(String[] args)
+	{    
+		int i=1;    
+		do{    
+        System.out.println(i);    
+		i++;    
+		}while(i<=10);    
+	}    
+}
