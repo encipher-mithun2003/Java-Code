eb28c4f10fe0a926b224ea6b2b55560576950688
Mithun Varshan S <114509306+encipher-mithun2003@users.noreply.github.com>
2024-03-18 21:31:52 +0530

    Create print_First_to_last.java

---
+++ Coding_Phase/print_First_to_last.java
@@ -0,0 +1,16 @@
+//PRINT FIRST TO LAST
+
+public class firsttolast   
+{  
+	public static void main(String[] args)   
+	{  
+		int first=1;
+		int last=5		
+		while(first<=last)   
+		{  
+			System.out.print(first);  
+			first=first+1;  
+		}  
+		
+	}  
+}
