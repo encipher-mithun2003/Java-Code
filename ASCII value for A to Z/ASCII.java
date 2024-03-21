b48cbebeef41736d98aafd270924c9a2dee4b561
Mithun Varshan S <114509306+encipher-mithun2003@users.noreply.github.com>
2024-03-21 22:20:26 +0530

    Create ASCII.java

---
+++ ASCII value for A to Z/ASCII.java
@@ -0,0 +1,7 @@
+public class ASCII {
+    public static void main(String[] args) {
+        for(char i='A';i<='Z';i++){
+            System.out.println("ASCII value of "+i+" is: "+(int)i);
+        }
+    }
+}
