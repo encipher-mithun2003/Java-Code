2f93652ce1ba53092820c30ff0c04d385da270fa
Mithun Varshan S <114509306+encipher-mithun2003@users.noreply.github.com>
2024-04-26 23:36:23 +0530

    Update ASCII_a_to_z.java

--- ASCII value for A to Z/ASCII_a_to_z.java
+++ ASCII value for A to Z/ASCII_a_to_z.java
@@ -2,7 +2,7 @@ public class ASCII {
     public static void main(String[] args) {
         // print ASCII value from A to Z
         for(char i='a';i<='z';i++){
-            System.out.println("ASCII value of "+i+" is: "+(int)i);
+            System.out.println("ASCII value of "+i+" is: "+(int)I);
         }
     }
 }
