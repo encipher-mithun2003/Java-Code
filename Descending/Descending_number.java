0051dbdb14a9afb2869c051a2fa754999b724b55
Mithun Varshan S <114509306+encipher-mithun2003@users.noreply.github.com>
2024-05-01 23:06:06 +0530

    Update Descending_number.java

--- Descending/Descending_number.java
+++ Descending/Descending_number.java
@@ -4,7 +4,7 @@ public class Descending {
     public static void main(String[] args)
     {
         Integer[] arr = { 13, 7, 6, 45, 21, 9, 2, 100 };
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(arr, Collections.reverseOrder()); // using collection function
         System.out.println("Modified arr[] : " + Arrays.toString(arr));
     }
 }
